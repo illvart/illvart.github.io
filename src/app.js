0e4688a5d5df4f4238c6aa0e265a318a0d81d2fc
illvart <illvart@protonmail.com>
2020-12-02 05:03:46 +0700

    up

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@
   } else {
     removeServiceWorkers();
   }
-
   function serviceWorkerIsSupported(hostname) {
     const allowedHostnames = ['__URL__', 'localhost', '127.0.0.1', '0.0.0.0'];
     return (
@@ -15,8 +14,45 @@
         hostname.endsWith('.github.io') ||
         hostname.endsWith('.netlify.app') ||
         hostname.endsWith('.vercel.app') ||
+        hostname.endsWith('.gitpod.io') ||
         hostname.endsWith('.codesandbox.io'))
     );
+  }
+  function ensureServiceWorker() {
+    const {pathname} = window.location;
+    const isFirstInstall = !navigator.serviceWorker.controller;
+    if (isFirstInstall) {
+      navigator.serviceWorker.addEventListener(
+        'controllerchange',
+        (event) => {
+          getHTML(pathname);
+          event.stopImmediatePropagation();
+        },
+        {once: true},
+      );
+    } else if (pathname !== '/') {
+      getHTML('/');
+    }
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      window.location.reload();
+    });
+    navigator.serviceWorker
+      .register('/sw.js', {scope: '/', updateViaCache: 'all'})
+      .then((reg) => {
+        console.log(`Service Worker Registered`);
+      })
+      .catch((err) => {
+        console.warn(`Service Worker Failed to Register`, err);
+      });
+  }
+  function removeServiceWorkers() {
+    if (!('serviceWorker' in navigator)) return;
+    return navigator.serviceWorker
+      .getRegistrations()
+      .then((all) => Promise.all(all.map((reg) => reg.unregister())))
+      .then((tasks) => {
+        if (tasks.length) window.location.reload();
+      });
   }
   async function getHTML(url, signal) {
     if (!(url.endsWith('/') || url.endsWith('.html'))) {
@@ -40,53 +76,21 @@
       throw e;
     }
   }
-  function ensureServiceWorker() {
-    const {pathname} = window.location;
-    const isFirstInstall = !navigator.serviceWorker.controller;
-    if (isFirstInstall) {
-      navigator.serviceWorker.addEventListener(
-        'controllerchange',
-        (event) => {
-          getHTML(pathname);
-          event.stopImmediatePropagation();
-        },
-        {once: true},
-      );
-    } else if (pathname !== '/') {
-      getHTML('/');
-    }
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-      window.location.reload();
-    });
-    navigator.serviceWorker
-      .register('/sw.js', {updateViaCache: 'all'})
-      .then((e) => {
-        console.log(`Well done, we supported offline!`);
-      })
-      .catch((e) => {
-        console.warn(`Oh no, we can't offline!`, e);
-      });
-  }
-  function removeServiceWorkers() {
-    if (!('serviceWorker' in navigator)) return;
-    return navigator.serviceWorker
-      .getRegistrations()
-      .then((all) => Promise.all(all.map((reg) => reg.unregister())))
-      .then((tasks) => {
-        if (tasks.length) window.location.reload();
-      });
-  }
 
   const id = document.getElementById('redirect');
   if (!id) return;
+
   let count = 11;
+
   function redirect() {
     count--;
     id.innerHTML = `<code>Redirect in ${count} seconds...</code>`;
     if (count != 0) setTimeout(redirect, 1000);
   }
   setTimeout(redirect, 1000);
+
   const disallowHostnames = ['localhost', '127.0.0.1', '0.0.0.0'];
+
   if (disallowHostnames.includes(window.location.hostname)) return;
   window.setTimeout(() => {
     window.location.replace(window.location.href.replace('__URL__', '__REDIRECT__'));
